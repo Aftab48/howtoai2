18b176e51eca05d68dd76376c6d6bffaaf4a1d53
Aftab48 <mdalam4884@gmail.com>
2024-08-10 08:28:24 +0530

    onLoad Animation added

--- components/transitionProvider.tsx
+++ components/transitionProvider.tsx
@@ -20,21 +20,21 @@ const TransitionProvider: React.FC<TransitionProviderProps> = ({
           className="h-screen w-screen fixed bg-black rounded-b-[100px] z-40"
           animate={{ height: "0vh" }}
           exit={{ height: "140vh" }}
-          transition={{ duration: 0.5, ease: "easeOut" }}
+          transition={{ duration: 1, ease: "easeOut" }}
         />
         <motion.div
           className="fixed m-auto top-0 bottom-0 left-0 right-0 text-white text-4xl md:text-6xl lg:text-8xl cursor-default z-50 w-fit h-fit"
           initial={{ opacity: 1 }}
           animate={{ opacity: 0 }}
           exit={{ opacity: 0 }}
-          transition={{ duration: 0.8, ease: "easeOut" }}
+          transition={{ duration: 1, ease: "easeOut" }}
         >
           {text}
         </motion.div>
         <motion.div
           className="h-screen w-screen fixed bg-black rounded-t-[100px] bottom-0 z-30"
           initial={{ height: "140vh" }}
-          animate={{ height: "0vh", transition: { delay: 0.5 } }}
+          animate={{ height: "0vh", transition: { delay: 1 } }}
         />
 
         <div className="h-[calc(100vh-6rem)]">{children}</div>
